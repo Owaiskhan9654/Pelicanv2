f632eaa085306fbac9a341ff0829e4533343a936
Owais Ahmad <owaiskhan9654@gmail.com>
2021-10-20 18:20:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,36 @@ app = Flask(__name__)
 @app.route('/predict', methods = ['POST'])
 def predict():
 
-    try:
-        sample_id = int(request.form['sample_id'])
-        result,final_prediction = feature_extraction_from_database(sample_id)
-        return render_template('index.html', tables=[result.to_html(classes='data',index = False)],final_prediction=final_prediction)
 
-    except:
-        sample_id = int(request.form['sample_id'])
-        return render_template('index.html', prediction_text= '{} cannot be Fetched from Database. Please get in touch with the team Development.'.format(sample_id))
+    sample_id = int(request.form['sample_id'])
+    result,final_prediction = feature_extraction_from_database(sample_id)
+    return render_template('index.html', tables=[result.to_html(classes='data',index = False)],final_prediction=final_prediction)
+
+    #except:
+    #  sample_id = int(request.form['sample_id'])
+
+    #  return render_template('index.html', prediction_text= '{} is Not a valid sample id as it cannot be Fetched from Database. Please get in touch with the team'.format(sample_id))
 
 
 
-@app.route('/login_sucess',methods = ['GET','POST'])
-def after_login():
-    if request.method == 'POST':
-        return render_template('index.html')
-    elif request.method == 'GET':
-        return render_template('login.html',error="Please Login")
+
 
 
 # route for handling the login page logic
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/',methods = ['GET','POST'])
 def login():
     error = None
+    a = {'owais.ahmed@canarydetect.com': 'Canary@2021', 'r.varsha@canarydetect.com': 'Canary@2021'}
+
     if request.method == 'POST':
-        if request.form['username'] != 'owais.ahmed@canarydetect.com'  or request.form['password'] != 'Canary@2021':
+        if request.form['username'] in a and a[request.form['username']] == request.form['password']:
+            return render_template('index.html')
+
+        else:
             error = 'Invalid Credentials. Please try again.'
-        else:    
-            return redirect(url_for('after_login'))
+
     return render_template('login.html', error=error)
+
 
 
 # start the server with the 'run()' method
